916d3b72b6cfb5936f4155ccc064e79c813ed6cc
Biliamin Popoola <genuinescripts@gmail.com>
2023-03-18 16:52:28 +0100

    changed week2task.php

---
+++ week2task.php
@@ -0,0 +1,16 @@
+<?php
+   $age = 22;
+   $pvc = true;
+   $ward = "20";
+   
+   if ($age < 18) {
+       echo "Not up to 18 years; not eligible to vote.";
+   } else if (!$pvc) {
+       echo "Invalid PVC; not eligible to vote.";
+   } else if ($ward !== "20") {
+       echo "Voter's does not belong to ward 20; not eligible to vote.";
+   } else {
+       echo "Voter is eligible to vote.";
+   }
+?>
+   
